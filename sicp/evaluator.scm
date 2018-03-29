20720c5fb94e46b1dfc50ae7bdd9f28922e3c384
Ou <yiqiou@amazon.com>
2018-03-28 23:07:49 -0700

    define helper procedure of evaluating application

--- sicp/evaluator.scm
+++ sicp/evaluator.scm
@@ -1,4 +1,4 @@
- ;;; try to implement the Metacircular evaluator of lisp
+;;; try to implement the Metacircular evaluator of lisp
 
 ;;; the evaluator contains two core function: eval and apply
 
@@ -15,7 +15,7 @@
 ;;; 4.expression type:
 ;;; 1)primitive: self-evaluation, look-up variables, quote
 ;;; 2)special form: assignment, definition, if, cond, lambda, begin
-;;; 3)combinations: procedure
+;;; 3)combinations: application
 
 ;;; questions:
 (define (eval exp env)
@@ -28,27 +28,42 @@
 	((lambda? exp) (eval-lambda exp env))
 	((begin? exp) (eval-begin exp env))
 	((cond? exp) (eval-cond exp env))
-	((procedure? exp) (eval-procedure exp) env)
+	((application? exp) (eval-application exp) env)
 	(else (error "Unknown expression type" exp))))
 
 ;;; eval-lambda:
 ;;; evaluation of lambda creates procedure object. we work on procedure abstract which has parameter, body and environment.
-;;; TODO: define 'make-procedure'; 
+;;; TODO: define "lambda" abstract which contains "lambda-parameters" and "lambda-body"
+;;;       define "procedure" abstract which contains 'make-procedure'; 
 (define (eval-lambda exp env)
   (make-procedure (lambda-parameters exp)
 		  (lambda-body exp)
 		  env))
 
 ;;; eval-begin:
-;;; we assume begin is an object which contains a list of expressions. (begin-actions give us the list of expressions
-;;;"(first-exp" gets the first exp in the list and "(rest-exp" returns the rest list of exps
-;;; TODO: define 'begin-actions', 'first-exp', 'rest-exp', 'last-exp'
+;;; we assume begin is an object which contains a list of expressions. begin-actions give us the list of expressions
+;;; TODO: define "begin" abstract which contains 'begin-actions', "begin-actions" returns a list of expression
 (define (eval-begin exp env)
   (eval-sequence (begin-actions exp) env))
 
 (define (eval-sequence exps env)
-  (cond ((last-exp? exps) (eval (first-exp exps) env))
-	(else (eval (first-exp exps) env)
-	      (eval-sequence (rest-exp exps) env))))
-  
-  
+  (cond ((null? (cdr exps)) (eval (car exps)))
+	(else (eval (car exps) env)
+	      (eval-sequence (cdr exps) env))))
+
+;;; eval-application:
+;;; application means function call. It contains operator (function name) and operands (arguments)
+;;; we would like to separate the evaluation process into two step. First evaluating operator and operands. Then evaluating
+;;; the triggering process which will be defined by "apply"
+;;; TODO: define "application" abstract which contains "application-operator" and "application-operands"
+(define (eval-application exp env)
+  (apply (eval (application-operator exp) env)
+	 (eval-list-of-values (application-operator exp) env)
+	 env))
+
+(define (eval-list-of-values  exps env)
+  (if (null? exps)
+      '()
+      (cons (eval (car exps) env)
+	    (eval-list-of-values (cdr env) env))))
+ 
