4b8c3391663e71123bf65641c365e073572ef54b
Ou <yiqiou@amazon.com>
2018-04-03 23:25:15 -0700

    continue working on representations

--- sicp/representations_v1.scm
+++ sicp/representations_v1.scm
@@ -24,3 +24,102 @@
   (cadr exp))
 
 
+;;; assignment: (set! a b)
+(define (assignment? exp)
+  (tagged-list? exp 'set!))
+
+(define (assignment-variable exp)
+  (cadr exp))
+
+(define (assignment-value exp)
+  (caddr exp))
+
+;;; definition:
+;;;  (define a b)
+;;;  (define (function_name a)
+;;;    (body))
+;;;  (define function_name
+;;;    (lambda (a) body))
+(define (definition? exp)
+  (tagged-list? exp 'define))
+
+(define (variable-definiton? exp)
+  (symbol? (cadr exp)))
+
+(define (function-definition exp)
+  (not (variable-definition exp)))
+
+(define (definition-variable exp)
+  (cond ((variable-definition? exp) (cadr exp))
+	((function-definition? exp) (caadr exp))
+	(else (error "Not a definition type" exp))))
+
+(define (definition-value exp)
+  (cond ((variable-definition? exp) (caddr exp))
+	((function-definition? exp)
+	 (make-lambda (cdadr exp)
+		      (cddr exp)))
+	(else (error "Not a defnition type" exp))))
+
+;;; lambda
+;;; (lambda (a) (+ a 1))
+(define (lambda? exp)
+  (tagged-list? exp 'lambda))
+
+(define (lambda-parameters exp)
+  (cadr exp))
+
+(define (lambda-body exp)
+  (cddr exp))
+
+(define (make-lambda parameters body)
+  (cons 'lambda (cons parameters body)))
+
+;;; if
+;;; (if (> a 1) 2 3)
+(define (if? exp)
+  (tagged-list? exp 'if))
+
+(define (if-predicate exp)
+  (cadr exp))
+
+(define (if-consequent exp)
+  (caddr exp))
+
+(define (if-alternative exp)
+  (if (null? cadddr exp)
+      'false
+      (cadddr exp)))
+
+(define (make-if predicate consequent alternative)
+  (list 'if predicate consequent alternative))
+
+;;; cond
+(define (cond? exp)
+  (tagged-list? exp 'cond))
+
+(define (cond-clauses exp)
+  (cdr cond))
+
+(define (cond->if exp)
+  (cond->if-iter (cond-clauses exp)))
+
+(define (clause-predicate clause)
+  (car clause))
+
+(define (clause-value clause)
+  (sequence->exp (cdr clause)))
+
+(define (else-clause? clause)
+  (eq? (car clause) 'else))
+
+(define (cond-if-iter clauses)
+  (let ((first-clause (car clauses))
+	(rest-clauses (cdr clauses)))
+    (let ((alternative
+	   (cond ((null? rest-clauses) 'false)
+		 ((else-clause? (car rest-clause)) (clause-value rest-clauses))
+		 (else (cond-if-iter rest-clauses)))))
+      (make-if (clause-predicate first-clause)
+	       (clause-value first-clause)
+	       alternative))))
